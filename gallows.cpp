2892672527d899d37dd4f287313c39d8ef29ceba
Julian Nicholls <juliannicholls29@gmail.com>
2025-09-13 20:31:13 +0100

    Update some colours in the galows. WIP

--- gallows.cpp
+++ gallows.cpp
@@ -1,7 +1,16 @@
 #include "gallows.h"
+#include "ansi.h"
+
+using namespace ANSI;
 
 const std::string top_with_rope =
     brown + top + " | /                  " + yellow + "|" + brown + "\n |/                   " + yellow + "|" + brown;
+
+const std::string top_with_head = top_with_rope + "\n |                  " + yellow + "-----" + brown +
+                                  "\n |                 " + yellow + "| o o |" + brown +
+                                  "\n |   "
+                                  "              " +
+                                  yellow + "|  o  |" + brown + "\n |                  " + yellow + "-----" + brown;
 
 const std::vector<std::string> TextGallows::gallows_text = {
     brown + "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" + land,
@@ -9,22 +18,23 @@ const std::vector<std::string> TextGallows::gallows_text = {
     brown + top + " |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |" + land,
     brown + top + " | /\n |/\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |" + land,
     top_with_rope + "\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |" + land,
-    top_with_rope + "\n |                  " + yellow + "-----" + brown + "\n |                 " + yellow + "| o o |" +
-        brown +
-        "\n |   "
-        "              " +
-        yellow + "|  o  |" + brown + "\n |                  " + yellow + "-----" + brown +
+    top_with_head +
         "\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n |\n "
         "|\n |\n |" +
         land,
 
-    brown + top + " | /   " + yellow + "|" + brown + "\n |/              |" + yellow + "|" + brown +
-        "\n |                  -----\n |                 | o o |\n |   "
-        "              |  o  |\n |                  -----\n |               ____|_|____ \n |              |           "
-        "|\n |              |     O     |\n |              |     O     |\n |              |     O     |\n |            "
-        "  |     O     |\n |              |           |\n |               -----------\n |\n |\n |\n |\n |\n |\n "
+    top_with_head + "\n |               " + yellow + "____|_|____" + brown + "\n |              " + yellow +
+        "|           |" + brown + "\n |              " + yellow + "|     " + light_green + "O" + yellow + "     |" +
+        brown + "\n |              " + yellow + "|     " + light_green + "O" + yellow + "     |" + brown +
+        "\n |              " + yellow + "|     " + light_green + "O" + yellow + "     |" + brown +
+        "\n |            "
+        "  " +
+        yellow + "|     " + light_green + "O" + yellow + "     |" + brown + "\n |              " + yellow +
+        "|           |" + brown + "\n |               " + yellow + "-----------\n" + brown +
+        " |\n |\n |\n |\n |\n |\n "
         "|\n |" +
         land,
+
     brown + top + " | /   " + yellow + "|" + brown + "\n |/              |" + yellow + "|" + brown +
         "\n |                  -----\n |                 | o o |\n |   "
         "              |  o  |\n |                  -----\n |           ___ ____|_|____ \n |          /  _|           "
