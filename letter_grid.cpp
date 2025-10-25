4cad1d6b6c39db9351c10a086c062844fb714110
Julian Nicholls <juliannicholls29@gmail.com>
2025-10-25 11:55:14 +0100

    Letter Grid highlights the letter under the mouse cursor and returns the letter if pressed.

--- letter_grid.cpp
+++ letter_grid.cpp
@@ -1,6 +1,31 @@
 #include "raylib.h"
 
 #include "letter_grid.h"
+#include "raylib_game.h"
+
+char LetterGrid::update()
+{
+    auto mouse_pos = GetMousePosition();
+
+    if (mouse_pos.x < left_ || mouse_pos.x > left_ + 13 * spacing_ || mouse_pos.y < top_ ||
+        mouse_pos.y > top_ + 2 * spacing_)
+    {
+        highlighted_ = -1;
+        return ' ';
+    }
+
+    const int line = (mouse_pos.y - top_) / spacing_; // 0 or 1
+    const int col = (mouse_pos.x - left_) / spacing_; // 0..12
+
+    highlighted_ = line * 13 + col;
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+    {
+        return 'a' + highlighted_;
+    }
+
+    return ' ';
+}
 
 void LetterGrid::draw(const Word &word) const
 {
@@ -9,8 +34,26 @@ void LetterGrid::draw(const Word &word) const
     for (int i = 0; i < 26; ++i)
     {
         ltr[0] = 'A' + i;
+
+        if (word.guessed(ltr[0]))
+            continue;
+
         const int line = i / 13;
         const int col = i % 13;
-        DrawText(ltr, left_ + col * spacing_, top_ + line * spacing_, font_size_, WHITE);
+        const float fSpacing = spacing_;
+        if (i == highlighted_)
+        {
+            DrawRectangleRounded(
+                {left_ + col * fSpacing, top_ + line * fSpacing, fSpacing, fSpacing}, 0.3f, 6, SKYBLUE);
+        }
+
+        // I'm not sure why the horizontal spacing needs an increase...
+        DrawTextEx(
+            game_.font(),
+            ltr,
+            {left_ + margin_ * 2 + col * fSpacing, top_ + margin_ + line * fSpacing},
+            font_size_,
+            2,
+            WHITE);
     }
 }
--- letter_grid.h
+++ letter_grid.h
@@ -3,21 +3,32 @@
 
 #include "word.h"
 
+class Game;
+
 class LetterGrid
 {
   public:
-    LetterGrid(int left, int top, int font_size = 16, int spacing = 24)
-        : left_{left}
+    LetterGrid(const Game &game, int left, int top, int font_size = 16, int spacing = 24)
+        : game_{game}
+        , left_{left}
         , top_{top}
         , font_size_{font_size}
-        , spacing_{spacing} {};
+        , spacing_{spacing}
+        , highlighted_(-1)
+        , margin_{(spacing - font_size) / 2} {};
 
+    char update();
     void draw(const Word &word) const;
 
   private:
+    const Game &game_;
+
     int left_;
     int top_;
     int font_size_;
     int spacing_;
+    int margin_;
+
+    int highlighted_;
 };
 #endif // LETTER_GRID_H
