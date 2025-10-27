1c9c24a63bc5ed78315befb98f06d128b2750fae
Julian Nicholls <juliannicholls29@gmail.com>
2025-10-27 20:02:16 +0000

    Introduce a game state and images for success and failure.

--- raylib_game.cpp
+++ raylib_game.cpp
@@ -8,8 +8,10 @@
 Game::Game(int width, int height, const std::string_view title)
     : words_{"../assets/words-2025-5-16.txt"}
     , gallows_{}
+    , state_{GameState::PLAYING}
+    , word_{words_.random()}
 {
-    // This MUST be done before anything else, not least loading texture images
+    // This MUST be done before anything else raylib-related, not least loading texture images
     InitWindow(width, height, std::string{title}.c_str());
     SetTargetFPS(60);
     SetExitKey(0); // Disable Esc to exit
@@ -17,43 +19,79 @@ Game::Game(int width, int height, const std::string_view title)
     gallows_ = std::make_unique<GraphicGallows>();
     font_ = std::make_unique<Font>(LoadFontEx("../assets/bloodcrow.ttf", 36, nullptr, 0));
     letter_grid_ = std::make_unique<LetterGrid>(*font_, 40, 680, 36, 40);
+    images_ = std::make_unique<ImageLoader>("../assets");
+}
+
+void Game::update()
+{
+    switch (state_)
+    {
+        using enum GameState;
+
+        case STARTING:
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                state_ = PLAYING;
+            break;
+
+        case PLAYING:
+            if (auto ch = letter_grid_->update(); ch != ' ')
+            {
+                if (!word_.guess(ch))
+                    gallows_->next();
+            }
+
+            if (word_.done())
+                state_ = SUCCESS;
+            else if (word_.bad_letters.size() == gallows_->stages() - 1)
+                state_ = FAILURE;
+            break;
+
+        case SUCCESS:
+        case FAILURE:
+            if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+                state_ = COMPLETE;
+            break;
+
+        case COMPLETE: break;
+    }
 }
 
 void Game::run()
 {
     int idx = 0;
 
-    Word word = words_.random();
+    auto hanged = [&]() { return; };
 
-    while (!WindowShouldClose())
+    while (!WindowShouldClose() && state_ != GameState::COMPLETE)
     {
-        BeginDrawing();
+        update();
+
+        if (state_ != GameState::COMPLETE)
+            BeginDrawing();
 
         ClearBackground(BLACK);
 
-        if (auto ch = letter_grid_->update(); ch != ' ')
+        switch (state_)
         {
-            if (!word.guess(ch))
-                gallows_->next();
+            using enum GameState;
+
+            case STARTING: DrawTexture(images_->at("lets-go"), 0, 300, WHITE); break;
+
+            case PLAYING:
+                gallows_->draw();
+                letter_grid_->draw(word_);
+                DrawTextEx(*font_, std::format("{}", word_).c_str(), {40, 620}, 36, 2, SKYBLUE);
+                break;
+
+            case SUCCESS: DrawTexture(images_->at("success-600"), 0, 300, WHITE); break;
+
+            case FAILURE: DrawTexture(images_->at("failure-600"), 0, 300, WHITE); break;
+            case COMPLETE: break;
         }
 
-        //        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
-        //        {
-        //            if (gallows_->stage() == 11)
-        //            {
-        //                gallows_->reset();
-        //            }
-        //            else
-        //            {
-        //                gallows_->next();
-        //            }
-        //        }
-
-        gallows_->draw();
-        letter_grid_->draw(word);
-
-        DrawTextEx(*font_, std::format("{}", word).c_str(), {40, 620}, 36, 2, SKYBLUE);
-
-        EndDrawing();
+        if (state_ != GameState::COMPLETE)
+            EndDrawing();
     }
+
+    CloseWindow();
 }
--- raylib_game.h
+++ raylib_game.h
@@ -6,6 +6,7 @@
 #include "raylib.h"
 
 #include "graphic_gallows.h"
+#include "images.h"
 #include "letter_grid.h"
 #include "wordlist.h"
 
@@ -22,10 +23,23 @@ class Game
     }
 
   private:
+    enum struct GameState
+    {
+        STARTING,
+        PLAYING,
+        SUCCESS,
+        FAILURE,
+        COMPLETE
+    };
+    void update();
+
+    GameState state_;
     Wordlist words_;
+    Word word_;
     std::unique_ptr<GraphicGallows> gallows_;
     std::unique_ptr<LetterGrid> letter_grid_;
     std::unique_ptr<Font> font_;
+    std::unique_ptr<ImageLoader> images_;
 };
 
 #endif // RAYLIB_GAME_H
